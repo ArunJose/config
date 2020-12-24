e14e5a60b911d33d673b8802bc7c58188252fc1e
ArunJose <12997751+ArunJose@users.noreply.github.com>
2020-12-25 01:26:15 +0530

    Update vim-vscode-keybinding.js

--- vim-vscode-keybinding.js
+++ vim-vscode-keybinding.js
@@ -1,9 +1,23 @@
-"vim.insertModeKeyBindings": [
-        {
-            "before": ["j", "j"],
-            "after": ["<esc>"]
-        }
-   ]
+  "vim.useSystemClipboard": true,
+  "vim.useCtrlKeys": false,
+  "vim.highlightedyank.enable": true,
+  "vim.normalModeKeyBindings": [
+    {
+      before: ["click"],
+      after: ["i"],
+    },
+  ],
+  "vim.insertModeKeyBindings": [
+    {
+      before: ["j", "j"],
+      after: ["<esc>"],
+    },
+    {
+      before: [";", ";"],
+      commands: ["cursorLineEnd"],
+    },
+  ],
+  "vim.startInInsertMode": true,
    
    
 //Ctrl+Shift+P settings.json
